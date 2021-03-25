4f855e5e1367a06bfc63005d6d1a6b70512399c3
hero-BA <suad.hero@gmail.com>
2021-03-25 19:53:24 +0100

    Added exercise: Set

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
+
+numberArray = new Set([...numberArray]);
+
+let uniqueArray = [...numberArray].sort((a, b) => a - b);
+
+console.log(uniqueArray);
+// Only change code above this line
+module.exports = { numberArray, uniqueArray };
